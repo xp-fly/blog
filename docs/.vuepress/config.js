a19cbcbe1cacb5aab4c11841ce2a06b1665019f8
xp-fly <793832050@qq.com>
2019-02-18 22:41:21 +0800

    修改构建的静态资源的前缀

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     head: [ // 注入到当前页面的 HTML <head> 中的标签
         ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
-    base: '/', // 这是部署到github相关的配置
+    base: '/blog/', // 这是部署到github相关的配置
     themeConfig: {
         nav:[ // 导航栏配置
             {text: 'Home', link: '/' },
@@ -24,7 +24,6 @@ module.exports = {
         sidebar: 'auto', // 侧边栏配置
         sidebarDepth: 2, // 侧边栏显示2级
         lastUpdated: 'Last Updated',
-        docsDir: 'docs',
     },
     markdown: {
         lineNumbers: true // 代码块显示行号
